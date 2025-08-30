c0ab63dc7f3f63c03240eda5e7af50aed919194c
Vitor Mesquita <90568058+vbma88@users.noreply.github.com>
2025-08-30 00:32:51 +0000

    Iniciando nivel novato

--- xadrez.c
+++ xadrez.c
@@ -1,10 +1,71 @@
 #include <stdio.h>
 
+int main() {
+    int i; // Variável 
+
+    // ..............................
+    // Definir passos 
+    // ..............................
+    int bispo_passos = 5;   // Movimentos do Bispo
+    int torre_passos = 5;   // Movimentos da Torre
+    int rainha_passos = 8;  // Movimentos da Rainha
+
+    // ..............................
+    // Movimentação do Bispo
+    // ..............................
+    // O Bispo anda na diagonal superior direita:
+    // ou seja, 1 passo para "Cima" + 1 passo para "Direita" em cada repetição.
+    printf("- Movimento do Bispo: -\n");
+    for (i = 0; i < bispo_passos; i++) {
+        printf(".........\n");
+        printf("Cima\n");
+        printf(".........\n");
+        printf("Direita\n");
+    }
+
+    printf("\n");
+
+    // ..............................
+    // Movimentação da Torre
+    // ..............................
+    // A Torre anda em linha reta. Aqui: 5 casas para a direita.
+    // Exemplo de uso do laço WHILE.
+    printf("- Movimento da Torre: -\n");
+    i = 0;
+    while (i < torre_passos) {
+        printf(".........\n");
+        printf("Direita\n");
+        i++;
+    }
+
+    printf("\n");
+
+    // ..............................
+    // Movimentação da Rainha
+    // ..............................
+    // A Rainha pode andar em qualquer direção.
+    // Aqui: 8 casas para a esquerda.
+    // Exemplo de uso do laço DO-WHILE.
+    printf("- Movimento da Rainha: -\n");
+    i = 0;
+    do {
+        printf(".........\n");
+        printf("Esquerda\n");
+        i++;
+    } while (i < rainha_passos);
+
+    printf("\n");
+    printf("...............................\n");
+    printf("- Fim da execução dos movimentos. -\n");
+    printf("...............................\n");
+
+    return 0; // Fim do programa
+}
+
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
@@ -28,5 +89,3 @@ int main() {
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
 
-    return 0;
-}
