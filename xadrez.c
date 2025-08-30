3bfee88029f26419cf12ae14940669c46b530f7c
Vitor Mesquita <90568058+vbma88@users.noreply.github.com>
2025-08-30 00:44:59 +0000

    alteração

--- xadrez.c
+++ xadrez.c
@@ -88,4 +88,3 @@ int main() {
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
-
